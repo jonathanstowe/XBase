8615c2a674290de78af587dd80f94d70aecaa39c
Jonathan Stowe <jns+git@gellyfish.co.uk>
2015-07-29 13:03:39 +0100

    Make version object

--- lib/XBase.pm
+++ lib/XBase.pm
@@ -2,39 +2,54 @@ use v6;
 
 class XBase {
 
-
     class Field {
 
     }
-    class Header {
-        has Field @.fields;
+
+    class VersionInfo {
         has Int $.version;
         has Bool $.memo;
         has Int $.sql;
         has Bool $.dbt;
 
+        multi submethod BUILD(Int :$ver!) {
+            $!version = $ver +& 0b00000111;
+            $!memo    = Bool($ver +& 0b00001000);
+            $!sql     = $ver +& 0b01110000;
+            $!dbt     = Bool($ver +& 0b10000000);
+        }
+
+        method Str() {
+            self.gist();
+        }
+
+        method gist() {
+            "Version {$!version} file { $!dbt ?? 'with' !! 'without' } DBT";
+        }
+
+    }
+
+    class Header {
+        has Field @.fields;
+
         has Date $.last-update;
         has Int $.records;
         has Int $!head-length;
+        has VersionInfo $.version;
 
         multi submethod BUILD(IO::Handle :$handle!) {
             my Buf $first-chunk = $handle.read(10);
             ( my Int $ver, my Int $yy, my Int $mm, my Int $dd, $!records, $!head-length ) = $first-chunk.unpack("CCCCLS");
 
             $!last-update = Date.new($yy + 1900, $mm, $dd);
-            $!version = $ver +& 0b00000111;
-            $!memo    = Bool($ver +& 0b00001000);
-            $!sql     = $ver +& 0b01110000;
-            $!dbt     = Bool($ver +& 0b10000000);
+            $!version = VersionInfo.new(:$ver);
         }
         method Str() {
             self.gist;
         }
         method gist() {
-            "Version {$!version} file { $!dbt ?? 'with' !! 'without' } DBT, {$!records} records. Last updated {$!last-update}";
+            $!version ~ ", {$!records} records. Last updated {$!last-update}";
         }
-
-
     }
 
     has $!filename;
